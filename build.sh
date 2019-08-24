e05d46d6c41df92c12665ee35718433a895e7d44
Albert <github@albert.sh>
2019-08-24 23:18:58 +0800

    Make build script re-runnable

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -ev
 
-mkdir dist
+mkdir -p dist
 
 #Build landing
 cp -r landing/* dist
 (cd dist && pandoc -t html -f gfm --css "pandoc.css" -s ../README.md -o index.html -V header-includes:"<link rel='shortcut icon' href='favicon.png' />" --metadata pagetitle="Headjack" --fail-if-warnings)
 
 # Build widget & login
-mkdir dist/login
+mkdir -p dist/login
 cp login/index.html dist/login
-mkdir dist/widget
+mkdir -p dist/widget
 cp widget/index.html dist/widget
 npm run build
 
