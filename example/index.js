2eca003084011e361fc813f0c7563cc06f4515dc
Albert <github@albert.sh>
2019-08-28 22:03:31 +0800

    Add button

--- example/index.js
+++ example/index.js
@@ -1,26 +1,27 @@
 import headjack from './headjack';
 
-console.log(headjack);
+document.getElementById("register").addEventListener("click", ()=>{ 
+	headjack.getAccount()
+		.then((account) => {
+			const {
+				address,
+				label
+			} = account;
 
-headjack.getAccount()
-.then((account) => {
-  const {
-    address,
-    label
-  } = account;
-
-  console.log('Provider address: ' + address);
-  console.log('Provider account label (Optional): ' + label);
-})
-.catch(({type, description, data}) => {
-  switch(type) {
-    case 'NO_PROVIDER':
-      console.log('No provider available');
-      break;
-    case 'CONNECTION_DENIED':
-      console.log('The user rejected the request');
-      break;
-  }
+			alert(address);
+			console.log('Provider address: ' + address);
+			console.log('Provider account label (Optional): ' + label);
+		})
+		.catch(({type, description, data}) => {
+			switch(type) {
+				case 'NO_PROVIDER':
+					console.log('No provider available');
+					break;
+				case 'CONNECTION_DENIED':
+					console.log('The user rejected the request');
+					break;
+			}
+		});
 });
 
 headjack.getProvider().then(console.log);
