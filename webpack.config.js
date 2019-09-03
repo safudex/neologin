e8d422640e5f5fab319d2aa9eb700884479930e1
Albert <github@albert.sh>
2019-09-04 04:13:09 +0800

    Compile provider into script

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 const path = require('path');
 
-module.exports = {
-	entry: {
-		login: './login/index.js',
-		widget: './widget/index.js',
-		example: './example/index.js',
-	},
-	mode: 'development',
-	devtool: 'inline-source-map',
-	output: {
-		filename: '[name]/bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	devServer: {
-		contentBase: './dist'
-	},
-};
+module.exports = [
+	{
+		entry: {
+			login: './login/index.js',
+			widget: './widget/index.js',
+			example: './example/index.js',
+		},
+		mode: 'development',
+		devtool: 'inline-source-map',
+		output: {
+			filename: '[name]/bundle.js',
+			path: path.resolve(__dirname, 'dist')
+		},
+		devServer: {
+			contentBase: './dist'
+		},
+	},{
+		entry: "./provider/index.js",
+		mode: 'development',
+		output: {
+			filename: 'headjack.js',
+			libraryTarget: 'window',
+			library: 'headjack',
+			libraryExport: 'default',
+			path: path.resolve(__dirname, 'dist')
+		}
+	}
+];
