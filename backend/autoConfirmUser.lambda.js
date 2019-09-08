e2bb4994c79b8d584cf1fab39eb979586aa93b0a
Albert <github@albert.sh>
2019-09-09 00:52:33 +0800

    Lambda function is already included in CF

--- backend/autoConfirmUser.lambda.js
+++
@@ -1,9 +0,0 @@
-exports.handler = (event, context, callback) => {
-
-    // Confirm the user
-    event.response.autoConfirmUser = true;
-
-    // Return to Amazon Cognito
-    callback(null, event);
-};
-
