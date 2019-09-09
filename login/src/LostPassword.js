7f5448d33e2742b7030e736d38e4fae3c2a5faad
Albert <github@albert.sh>
2019-09-10 06:57:55 +0800

    Re-do lostpassword flow

--- login/src/LostPassword.js
+++ login/src/LostPassword.js
@@ -10,106 +10,191 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import PropTypes from 'prop-types';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+const styles = (theme => ({
+	'@global': {
+		body: {
+			backgroundColor: theme.palette.common.white,
+		},
+	},
+	paper: {
+		marginTop: theme.spacing(8),
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.secondary.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 }));
 
-export default function SignIn(props) {
-  const classes = useStyles();
+class LostPassword extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			goBackClick: props.goBackClick,
+			wrongEmail: false,
+			wrongCode: false,
+			wrongPassword: false,
+			email: "",
+			password: "",
+			code: "",
+			privkey: "",
+			handleLogin: props.handleLogin,
+			emailCompleted: false,
+			codeCompleted: false,
+		};
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Lost Password
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Typography component="p">
-            Swas
-			<br />
-            salkdsalk
-          </Typography>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="privkey"
-            label="Private Key"
-            name="privkey"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Reset password
-          </Button>
-          <Button
-            fullWidth
-            variant="contained"
-            color="secondary"
-	    onClick={props.goBackClick}
-          >
-            Go Back
-          </Button>
-        </form>
-      </div>
-    </Container>
-  );
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleInputChange(event) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
+	}
+
+	async handleSubmit(event){
+		event.preventDefault();
+		if(!this.state.emailCompleted){
+			Auth.forgotPassword(this.state.email)
+				.then(data => {
+					this.setState({
+						emailCompleted: true
+					});
+					console.log(data)
+				})
+				.catch(err => {
+					this.setState({
+						wrongEmail: "Email is not registered"
+					});
+					console.log(err)
+				});
+		} else if(!this.state.codeCompleted){
+			Auth.forgotPasswordSubmit(this.state.email, this.state.code, this.state.password)
+				.then(data => {
+					this.setState({
+						emailCompleted: true
+					});
+					console.log(data)
+				})
+				.catch(err => {
+					this.setState({
+						wrongCode: "Wrong code"
+					});
+					console.log(err)
+				});
+		} else {
+
+		}
+	}
+
+	render(){
+		let body = null;
+		if(!this.state.emailCompleted){
+			body=<div>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="email"
+					label="Email"
+					name="email"
+					value={this.state.email}
+					autoFocus
+					onChange={this.handleInputChange}
+				/>
+			</div>
+		} else {
+			body=<div>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="privkey"
+					label="Private Key"
+					name="privkey"
+					autoFocus
+					value={this.state.privkey}
+					onChange={this.handleInputChange}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					name="password"
+					label="Password"
+					type="password"
+					id="password"
+					autoComplete="current-password"
+					value={this.state.password}
+					onChange={this.handleInputChange}
+				/>
+			</div>
+		}
+
+		const { classes } = this.props;
+		return (
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<div className={classes.paper}>
+					<Avatar className={classes.avatar}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Lost Password
+					</Typography>
+					<form className={classes.form} onSubmit={this.handleSubmit}>
+						{body}
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							className={classes.submit}
+						>
+							Reset password
+						</Button>
+						<Button
+							fullWidth
+							variant="contained"
+							color="secondary"
+							onClick={this.state.goBackClick}
+						>
+							Go Back to Sign In
+						</Button>
+					</form>
+				</div>
+			</Container>
+		);
+	}
 }
+
+
+LostPassword.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LostPassword);
