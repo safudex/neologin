936314f269ecf23b8f1230a2392f16c7714879d0
Albert <github@albert.sh>
2019-09-10 07:07:40 +0800

    Build bundle

--- build.sh
+++ build.sh
@@ -8,8 +8,16 @@ cp -r landing/* dist
 (cd dist && pandoc -t html -f gfm --css "pandoc.css" -s ../README.md -o index.html --metadata pagetitle="Headjack" --fail-if-warnings)
 
 if [ $1 == "production" ]; then
+	# Build provider bundle
+	cd provider
+	npm install
+	webpack
+	cd ..
+
+	# Build login page
 	cd login
 	yarn install
 	CI=false yarn build
 	mv build ../dist/login
+	cd ..
 fi
