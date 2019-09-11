85b5026d622efd02109a90419d4204ac1af58be1
Albert <github@albert.sh>
2019-09-11 20:05:55 +0800

    Finish lost password flow

--- login/src/LostPassword.js
+++ login/src/LostPassword.js
@@ -79,7 +79,8 @@ class LostPassword extends React.Component {
 			Auth.forgotPassword(this.state.email)
 				.then(data => {
 					this.setState({
-						emailCompleted: true
+						emailCompleted: true,
+						wrongEmail: false
 					});
 					console.log(data)
 				})
@@ -89,13 +90,41 @@ class LostPassword extends React.Component {
 					});
 					console.log(err)
 				});
-		} else if(!this.state.codeCompleted){
+		} else {
+			if(this.state.password.length<=6){
+				alert("Password is too weak");
+				return;
+			}
+			if(this.state.password.privkey<=100){
+				alert("Wrong privkey");
+				return;
+			}
 			Auth.forgotPasswordSubmit(this.state.email, this.state.code, this.state.password)
-				.then(data => {
+				.then(async data => {
 					this.setState({
-						emailCompleted: true
+						codeCompleted: true
 					});
 					console.log(data)
+
+					try{
+						const user = await Auth.signIn(this.state.email, this.state.password);
+						if (user.challengeName === 'SMS_MFA' ||
+							user.challengeName === 'SOFTWARE_TOKEN_MFA') {
+							const code = prompt("Enter "+user.challengeName+" code:");
+							const loggedUser = await Auth.confirmSignIn(
+								user,   // Return object from Auth.signIn()
+								code,   // Confirmation code
+								user.challengeName // MFA Type e.g. SMS_MFA, SOFTWARE_TOKEN_MFA
+							);
+							updatePrivkey(loggedUser, this.state.privkey, this.state.password);
+						} else if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
+						} else if (user.challengeName === 'MFA_SETUP') {
+						} else {
+							updatePrivkey(user, this.state.privkey, this.state.password);
+						}
+					} catch (err) {
+						alert("An error has ocurred, please contact support@neologin.io and provide them with the following report:\n"+JSON.stringify(err))
+					}
 				})
 				.catch(err => {
 					this.setState({
@@ -103,8 +132,6 @@ class LostPassword extends React.Component {
 					});
 					console.log(err)
 				});
-		} else {
-
 		}
 	}
 
@@ -122,6 +149,8 @@ class LostPassword extends React.Component {
 					name="email"
 					value={this.state.email}
 					autoFocus
+					error={this.state.wrongEmail? true : null}
+					helperText={this.state.wrongEmail}
 					onChange={this.handleInputChange}
 				/>
 			</div>
@@ -132,11 +161,26 @@ class LostPassword extends React.Component {
 					margin="normal"
 					required
 					fullWidth
+					id="code"
+					label="Code"
+					name="code"
+					autoFocus
+					value={this.state.code}
+					error={this.state.wrongCode? true : null}
+					helperText={this.state.wrongCode? this.state.wrongCode : "Input the code received through email."}
+					onChange={this.handleInputChange}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
 					id="privkey"
 					label="Private Key"
 					name="privkey"
 					autoFocus
 					value={this.state.privkey}
+					helperText= "You can find the code for this field in the file that was downloaded when you registered for NeoLogin. Please make sure that the code you are inputting is effecively the same that you downloaded, as it will become the private key associated with this account."
 					onChange={this.handleInputChange}
 				/>
 				<TextField
@@ -145,15 +189,24 @@ class LostPassword extends React.Component {
 					required
 					fullWidth
 					name="password"
-					label="Password"
+					label="New Password"
 					type="password"
 					id="password"
 					autoComplete="current-password"
 					value={this.state.password}
 					onChange={this.handleInputChange}
 				/>
-			</div>
+					</div>
 		}
+
+		const goBackButton=<Button
+							fullWidth
+							variant="contained"
+							color="secondary"
+							onClick={this.state.goBackClick}
+						>
+							Go Back to Sign In
+						</Button>
 
 		const { classes } = this.props;
 		return (
@@ -166,6 +219,13 @@ class LostPassword extends React.Component {
 					<Typography component="h1" variant="h5">
 						Lost Password
 					</Typography>
+					{ this.state.codeCompleted?
+					<div>
+						<Typography component="h2" variant="h4">
+							Password recovery succeeded
+						</Typography>
+						{goBackButton}
+					</div> :
 					<form className={classes.form} onSubmit={this.handleSubmit}>
 						{body}
 						<Button
@@ -177,21 +237,30 @@ class LostPassword extends React.Component {
 						>
 							Reset password
 						</Button>
-						<Button
-							fullWidth
-							variant="contained"
-							color="secondary"
-							onClick={this.state.goBackClick}
-						>
-							Go Back to Sign In
-						</Button>
+						{goBackButton}
 					</form>
+					}
 				</div>
 			</Container>
 		);
 	}
 }
 
+async function updatePrivkey(user, privkey, password){
+	const encryptedPrivkey = await encrypt(privkey, password);
+	let result = await Auth.updateUserAttributes(user, {
+		"custom:privkey": encryptedPrivkey,
+	});
+}
+
+// TODO: Eliminate replication of this function with SignUp.js
+function encrypt(plaintext, key){
+	return import("cryptr")
+		.then(( Cryptr ) => {
+			Cryptr = Cryptr.default;
+			return new Cryptr(key).encrypt(plaintext);
+		});
+}
 
 LostPassword.propTypes = {
 	classes: PropTypes.object.isRequired,
