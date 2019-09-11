c26054e096f8b07f4237772401854cf118d7034c
Albert <github@albert.sh>
2019-09-11 20:07:37 +0800

    Add privacy policy

--- login/src/SignUp.js
+++ login/src/SignUp.js
@@ -255,7 +255,7 @@ class SignUp extends React.Component {
 								<Grid item xs={12}>
 									<FormControlLabel
 										control={<Checkbox value="privacyPolicy" color="primary" name="privacyPolicy" checked={this.state.privacyPolicy} onChange={this.handleInputChange} />}
-										label={<span>I agree with <Link href="/privacy">NeoLogin's privacy policy</Link></span>}
+										label={<span>I agree with <Link href="/privacy-policy.pdf">NeoLogin's privacy policy</Link></span>}
 									/>
 									{this.state.privacyPolicy? null :
 									<FormHelperText error={true}>You must agree to the privacy policy</FormHelperText>
