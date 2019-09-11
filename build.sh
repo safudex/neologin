7af3f7cfcb86420bc3d77f4f527802bfab0a54d4
Albert <github@albert.sh>
2019-09-12 06:23:23 +0800

    Improve build

--- build.sh
+++ build.sh
@@ -4,7 +4,45 @@ set -ev
 mkdir -p dist
 
 #Build landing
-cp -r landing/* dist
+if [ $1 == "production" ]; then
+	cp -r landing/* dist
+fi
+
+# Build login page
+cd login
+yarn install
+if [ $1 == "dev" ]; then
+	PORT=3001 yarn start &
+fi
+if [ $1 == "production" ]; then
+	CI=false yarn build
+	mv build ../dist/login
+fi
+cd ..
+
+# Build test page
+cd test/test
+npm install
+if [ $1 == "dev" ]; then
+	PORT=3000 npm start &
+fi
+if [ $1 == "production" ]; then
+	CI=false yarn build
+	#mv build ../dist/login
+fi
+cd ..
+
+# Build widget
+cd widget/widget
+npm install
+if [ $1 == "dev" ]; then
+	PORT=3002 npm start &
+fi
+if [ $1 == "production" ]; then
+	CI=false yarn build
+	#mv build ../dist/login
+fi
+cd ..
 
 if [ $1 == "production" ]; then
 	# Build provider bundle
@@ -18,12 +56,5 @@ if [ $1 == "production" ]; then
 	yarn install
 	yarn dist
 	mv build ../dist/api
-	cd ..
-
-	# Build login page
-	cd login
-	yarn install
-	CI=false yarn build
-	mv build ../dist/login
 	cd ..
 fi
