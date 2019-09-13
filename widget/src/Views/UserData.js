b7d9e6894fa952418450d97035e81585ac3a723e
urlchange <salah.karim@e-campus.uab.cat>
2019-09-13 19:14:11 +0200

    applied layout widget

--- widget/src/Views/UserData.js
+++ widget/src/Views/UserData.js
@@ -1,27 +1,52 @@
 import React from 'react';
+import logo from '../logo.svg';
+import logo2 from '../logo2.png';
+import Grid from '@material-ui/core/Grid';
+
+import './LoginButton.css'
 
 function UserData({ account, closeWidget }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Your address is</h1>
-        <p style={{ color: 'black' }}>
-          {account._address}
-        </p>
-        <button onClick={() => closeWidget()}
-          style={{
-            position: 'absolute',
-            top: '0',
-            right: '0',
-            marginTop: '2.5rem',
-            marginRight: '1em',
-            padding: '0 .25em',
-            border: 'none',
-            fontSize: '1.25em',
-            color: '#777',
-            transition: 'color .3s ease'
-          }}>X</button>
-      </header>
+    <div>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        style={{ textAlign: 'center', padding: '8px 0', background: '#fff' }}
+      >
+        <Grid item xs={1}></Grid>
+        <Grid item xs={10}>
+          <img src={logo2} style={{ height: '1em' }} />
+        </Grid>
+        <Grid item xs={1}>
+          <Grid
+            container
+            justify="flex-start"
+            alignItems="flex-start"
+          >
+            <Grid item>
+              <span className='closeButton' onClick={() => closeWidget()}>
+                ✖
+              </span>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid >
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        style={{ padding: '1em', textAlign:'center' }}
+      >
+        <Grid item xs>
+          <img src={logo} width='65px' />
+        </Grid>
+        <Grid item xs>
+          <p style={{ fontSize: '0.85em' }}>Your address is { account._address }</p>
+        </Grid>
+      </Grid >
     </div>
   );
 }
