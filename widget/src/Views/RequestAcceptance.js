a117151032d399e24c3a56dbc3442a53b6e164c1
urlchange <salah.karim@e-campus.uab.cat>
2019-09-18 19:35:33 +0200

    added ask user for acceptance and acumulate messages

--- widget/src/Views/RequestAcceptance.js
+++ widget/src/Views/RequestAcceptance.js
@@ -4,56 +4,74 @@ import logo from '../logo.svg';
 import logo2 from '../logo2.png';
 
 import Brand from './Brand'
+import ReactDOM from 'react-dom'
 
 import './styles.css'
 
-function RequestAcceptance({ message, resolve, reject, closeWidget }) {
-  return (
-    <div>
-      <Brand closeWidget={closeWidget}/>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        style={{ height: '100%', padding: '1em' }}
-      >
-        {/* <Grid item xs>
-          <img src={logo} width='65px' />
-        </Grid> */}
-        <Grid item xs>
-          <p style={{ fontSize: '0.85em' }}>Do you accept this transacction?</p>
-        </Grid>
-        <Grid item xs style={{ width: '100%' }}>
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            spacing={2}
-          >
-            <Grid item xs>
-              <button className='buttonContinue buttonAccept' onClick={() => { resolve(); closeWidget() }}>
-                Accept
+//({ transaction, message, resolve, reject, closeWidget }) {
+class RequestAcceptance extends React.Component {
+
+  unmountComponent = () => {
+    ReactDOM.unmountComponentAtNode(window.document.getElementById(this.props.contid))
+  }
+
+  componentWillUnmount() {
+    window.document.getElementById(this.props.contid).remove();
+    this.props.closeRequest()
+    this.props.closeWidget()
+  }
+
+  render() {
+    return (
+      <div>
+        <Brand closeWidget={this.unmountComponent} reqNumber={parseInt(this.props.contid.split('-')[1]) + 1} />
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          style={{ height: '100%', padding: '1em' }}
+        >
+          {
+            this.props.transaction ?
+              <Grid item xs>
+                <img src={logo} height='65px' />
+              </Grid> : null
+          }
+
+          <Grid item xs>
+            <p style={{ fontSize: '0.85em' }}>{this.props.transaction ? 'Do you accept that transaction?' : this.props.message}</p>
+          </Grid>
+          <Grid item xs style={{ width: '100%' }}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid item xs>
+                <button className='buttonContinue buttonAccept' onClick={() => {
+                  this.props.resolve('resolveincomponent')
+                  this.unmountComponent()
+                }}>
+                  Accept
+                </button>
+              </Grid>
+              <Grid item xs>
+                <button className='buttonContinue buttonReject' onClick={() => {
+                  this.props.reject()
+                  this.unmountComponent()
+                }}>
+                  Reject
             </button>
-            </Grid>
-            <Grid item xs>
-              <button className='buttonContinue buttonReject' onClick={() => { reject(); closeWidget() }}>
-                Reject
-            </button>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </Grid >
-    </div>
-  );
+        </Grid >
+      </div >
+    );
+  }
 }
-
-{/* <Button variant="contained" color="primary" onClick={() => { resolve(); closeWidget() }}>
-  Accept
-          </Button>
-  <Button variant="contained" color="secondary" onClick={() => { reject(); closeWidget() }}>
-    Reject
-      </Button> */}
 
 export default RequestAcceptance;
