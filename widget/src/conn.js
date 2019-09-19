20574bd86593afa3ca309b08d959da63071007ca
Albert <github@albert.sh>
2019-09-20 03:44:32 +0800

    Merge @urlchange stuff

--- widget/src/conn.js
+++ widget/src/conn.js
@@ -104,29 +104,40 @@ const providerInfo = {
 	version: 'v0.1',
 	compatibility: [],
 	extra: {
-		theme: '',
-		currency: ''
+		theme: null,
+		currency: null
 	}
 };
 
-function getProvider() {//To Do: case CONNECTION_DENIED
+function getProvider() {
 	return new Promise((resolve, reject) => {
-		if (providerInfo)
+		requestAcceptance('Do you want to give access to your provider information?', false).then(() => {
 			resolve(providerInfo)
-		else
+		}).catch(() => {
 			reject({
-				type: 'NO_PROVIDER',
-				description: 'No provider available.',
-				data: ''
-			})
+				type: 'CONNECTION_DENIED',
+				description: 'The user rejected the request to connect with your dApp.',
+				data: null
+			});
+		})
 	})
 }
 
 function getNetworks() {
-	return Promise.resolve({
-		networks: ["MainNet", "TestNet"],
-		defaultNetwork: defaultNetwork
-	});
+	return new Promise((resolve, reject) => {
+		requestAcceptance('Do you want to give access to your network information?', false).then(() => {
+			resolve({
+				networks: ["MainNet", "TestNet"],
+				defaultNetwork: defaultNetwork
+			})
+		}).catch(() => {
+			reject({
+				type: 'CONNECTION_DENIED',
+				description: 'The user rejected the request to connect with your dApp.',
+				data: null
+			});
+		})
+	})
 }
 
 function getAccount(...args) {
@@ -147,44 +158,206 @@ function getAccount(...args) {
 }
 
 function getPublicKey() {
-	return Promise.resolve({
-		address: acct.address,
-		publicKey: acct.publicKey
-	});
+	return new Promise((resolve, reject) => {
+		requestAcceptance('Do you want to give access to your public key?', false).then(() => {
+			resolve({
+				address: acct.address,
+				publicKey: acct.publicKey
+			})
+		}).catch(() => {
+			reject({
+				type: 'CONNECTION_DENIED',
+				description: 'The user rejected the request to connect with your dApp.',
+				data: ''
+			});
+		})
+	})
 }
 
-function getBalance(balanceArgs) { }
+function getBalance(balanceArgs) {
+	return new Promise((resolve, reject) => {
+		requestAcceptance('Do you want to give access to your balance?', false).then(() => {
+			resolve()
+		}).catch(() => {
+			reject({
+				type: 'CONNECTION_DENIED',
+				description: 'The user rejected the request to connect with your dApp.',
+				data: ''
+			});
+		})
+	})
+}
 
-function getStorage(storageArgs) { } //{ scriptHash: string, key: string, network?: string }){
+function getStorage(storageArgs) {
+	return new Promise((resolve, reject) => {
+		if (!acct)
+			reject({
+				type: 'CONNECTION_REFUSED',
+				description: 'Connection dApp not connected. Please call the "connect" function.',
+				data: ''
+			})
+		else
+			//foo().then(() => resolve({ result: 'hello world' })).catch(() => {
+			//	reject({
+			//		type: 'RPC_ERROR',
+			//		description: 'There was an error when broadcasting this transaction to the network.',
+			//		data: ''
+			//	});
+			//})
+			resolve({ result: 'hello world' })
+	})
+} //{ scriptHash: string, key: string, network?: string }){
 
-function invokeRead(invokeArgs) { }
+function invokeRead(invokeArgs) {
+	return new Promise((resolve, reject) => {
+		if (!acct)
+			reject({
+				type: 'CONNECTION_REFUSED',
+				description: 'Connection dApp not connected. Please call the "connect" function.',
+				data: ''
+			})
+		else
+			//foo().then(() => resolve({ ... })).catch(() => {
+			//	reject({
+			//		type: 'RPC_ERROR',
+			//		description: 'There was an error when broadcasting this transaction to the network.',
+			//		data: ''
+			//	});
+			//})
+			resolve({
+				script: '8h89fh398f42f.....89hf2894hf9834',
+				state: 'HALT, BREAK',
+				gas_consumed: '0.13',
+				stack: [
+					{
+						type: 'Integer',
+						value: '1337'
+					}
+				]
+			})
+	})
+}
 
-function getBlock(blockArgs) { }
+function getBlock(blockArgs) {
+	//return new Promise((resolve, reject) => {
+	//	foo().then(() => resolve({ ... })).catch(() => {
+	//		reject({
+	//			type: 'RPC_ERROR',
+	//			description: 'There was an error when broadcasting this transaction to the network.',
+	//			data: ''
+	//		});
+	//	})
+	//})
+}
 
 function getBlockHeight(blockHeightArgs) {
 	//return client.getBlockCount();
+
+	//return new Promise((resolve, reject) => {
+	//	foo().then(() => resolve({ "result": 2619690 })).catch(() => {
+	//		reject({
+	//			type: 'RPC_ERROR',
+	//			description: 'There was an error when broadcasting this transaction to the network.',
+	//			data: ''
+	//		});
+	//	})
+	//})
 }
 
-function getTransaction(txArgs) { }
+function getTransaction(txArgs) {
+	//return new Promise((resolve, reject) => {
+	//	foo().then(() => resolve({ ... })).catch(() => {
+	//		reject({
+	//			type: 'RPC_ERROR',
+	//			description: 'There was an error when broadcasting this transaction to the network.',
+	//			data: ''
+	//		});
+	//	})
+	//})
+}
 
-function getApplicationLog(appLogArgs) { }
+function getApplicationLog(appLogArgs) {
+	//return new Promise((resolve, reject) => {
+	//	foo().then(() => resolve({ ... })).catch(() => {
+	//		reject({
+	//			type: 'RPC_ERROR',
+	//			description: 'There was an error when broadcasting this transaction to the network.',
+	//			data: ''
+	//		});
+	//	})
+	//})
+}
 
 function send(sendArgs) {
 	return new Promise((resolve, reject) =>
-		requestAcceptance(JSON.stringify(sendArgs), true).then(() =>
+		requestAcceptance(JSON.stringify(sendArgs), true).then(() =>//TODO DIFFERENT FOR TRANSACTION
 			alert('sent!')
+			//foo().then(() => resolve( { txid:'', nodeUrl:'' } )).catch((err) => {
+			//	let err_response = {}
+			//	err_response.type = error
+			//	switch (error) {
+			//		case 'SEND_ERROR':
+			//			err_response.description = 'There was an error when broadcasting this transaction to the network.'
+			//			break;
+			//		case 'MALFORMED_INPUT':
+			//			err_response.description = 'The receiver address provided is not valid.'
+			//			break;
+			//		case 'INSUFFICIENT_FUNDS':
+			//			err_response.description = 'The user has insufficient funds to execute this transaction.'
+			//			break;
+			//	}
+			//	reject(err_response);
+			//})
 		).catch(() =>
-			reject('not sent!')
+			reject({
+				type: 'CANCELED',
+				description: 'There was an error when broadcasting this transaction to the network.',
+				data: ''
+			})
 		)
 	)
 }
 
 function invoke(invokeArgs) {
-	requestAcceptance(JSON.stringify(invokeArgs)).then(() => alert('sent!'));
+	return new Promise((resolve, reject) =>
+		requestAcceptance(JSON.stringify(invokeArgs)).then(() =>
+			alert('sent!')
+			//	foo().then(() => resolve({ ... })).catch(() => {
+			//		reject({
+			//			type: 'RPC_ERROR',
+			//			description: 'There was an error when broadcasting this transaction to the network.',
+			//			data: ''
+			//		});
+			//	})
+		).catch(() =>
+			reject({
+				type: 'CANCELED',
+				description: 'There was an error when broadcasting this transaction to the network.',
+				data: ''
+			})
+		)
+	)
 }
 
 function invokeMulti(invokeMultiArgs) {
-	requestAcceptance(JSON.stringify(invokeMultiArgs)).then(() => alert('sent!'));
+	return new Promise((resolve, reject) =>
+		requestAcceptance(JSON.stringify(invokeMultiArgs)).then(() =>
+			alert('sent!')
+			//	foo().then(() => resolve({ ... })).catch(() => {
+			//		reject({
+			//			type: 'RPC_ERROR',
+			//			description: 'There was an error when broadcasting this transaction to the network.',
+			//			data: ''
+			//		});
+			//	})
+		).catch(() =>
+			reject({
+				type: 'CANCELED',
+				description: 'There was an error when broadcasting this transaction to the network.',
+				data: ''
+			})
+		)
+	)
 }
 
 function signMessage(signArgs) {
@@ -197,18 +370,39 @@ function signMessage(signArgs) {
 				const concatenatedString = lengthHex + parameterHexString;
 				const messageHex = '010001f0' + concatenatedString + '0000';
 
+				//reject({
+				//	type: 'UNKNOWN_ERROR',
+				//	description: 'There was an unknown error.',
+				//	data: ''
+				//})
 				resolve({
 					publicKey: acct.publicKey, // Public key of account that signed message
 					message: signArgs.message, // Original message signed
 					salt: salt, // Salt added to original message as prefix, before signing
 					data: messageHex, // Signed message
 				});
-			});
-		});
+
+			})
+		})
 }
 
 function deploy(deployArgs) {
-	requestAcceptance(JSON.stringify(deployArgs)).then(() => alert('sent!'));
+	return requestAcceptance(JSON.stringify(deployArgs))
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				alert('sent!')
+
+				//reject({
+				//	type: 'UNKNOWN_ERROR',
+				//	description: 'There was an unknown error.',
+				//	data: ''
+				//})
+				resolve({
+					txid: null,
+					nodeUrl: null
+				})
+			})
+		})
 }
 
 function closeWidget() {
