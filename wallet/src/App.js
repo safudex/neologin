bec1afaf1b697719485f0528c067f16825ec5002
urlchange <salah.karim@e-campus.uab.cat>
2019-09-27 23:24:53 +0200

    redesign wallet

--- wallet/src/App.js
+++ wallet/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import WalletView from './Components/WalletView'
-
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import Divider from '@material-ui/core/Divider';
-
-import walletIcon from './imgs/wallet.png'
-import sendIcon from './imgs/mail.png'
-import keyIcon from './imgs/key.png'
-import settingsIcon from './imgs/settings.png'
+import QRCode from 'qrcode'
+import SvgIcon from '@material-ui/core/SvgIcon';
 
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import MyWallet from './views/MyWallet';
-import SendWallet from './views/SendWallet';
-import SecurityWallet from './views/SecurityWallet';
+
+import neologin from 'neologin'
+
+import AssetRow from './Components/AssetRow'
+
+import { server } from './config';
+
+function drawQR(address) {
+  let canvas = document.getElementById('addressQR')
+  QRCode.toCanvas(canvas, address, (error) => {
+    if (error) {
+      console.error(error);
+      alert(error);
+    }
+  })
+}
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -35,7 +39,8 @@ function App() {
 
   const classes = useStyles();
   const [network, setNetwork] = React.useState('TestNet');
-  const [tabSelected, setTabSelected] = React.useState(2);
+  const [address, setAddress] = React.useState('');
+  const [mainBalance, setBalance] = React.useState([]);
   const [isMobile, setIsMobile] = React.useState(document.documentElement.clientWidth < 576);
 
   function changeStyles(w, h) {
@@ -53,106 +58,124 @@ function App() {
 
   function handleChange(event) {
     setNetwork(event.target.value);
+    getBalance(address, event.target.value)
+  }
+
+  useEffect(() => {
+    neologin.getAccount()
+      .then(({ address }) => {
+        drawQR(address)
+        setAddress(address)
+        getBalance(address, network)
+      })
+      .catch(({ type, description, data }) => {
+        setAddress(type)
+        switch (type) {
+          case "NO_PROVIDER":
+            console.log('No provider available.');
+            break;
+          case "CONNECTION_DENIED":
+            console.log('The user rejected the request to connect with your dApp');
+            break;
+        }
+      })
+  }, [])
+
+
+
+  function getBalance(address, network) {
+    neologin.getBalance({
+      params: {
+        address: address
+      },
+      network: network,
+    })
+      .then((results) => {
+        Object.keys(results).forEach(address => {
+          const balances = results[address];
+          setBalance(balances)
+          console.log(results)
+        });
+      })
+      .catch(({ type, description, data }) => {
+        switch (type) {
+          case "NO_PROVIDER":
+            console.log('No provider available.');
+            break;
+          case "CONNECTION_DENIED":
+            console.log('The user rejected the request to connect with your dApp');
+            break;
+        }
+      });
+
   }
 
   return (
-    <div>
-      <div className="gradientBg"></div>
-      <Grid container direction="column" justify="center" alignItems="center" className="centerPaper">
-        <Grid item style={{ width: '75%' }}>
-          <Paper style={{ padding: '1rem', marginBottom: '5rem' }}>
-            <Grid container direction={isMobile ? 'column' : 'row'} justify="space-between" alignItems="center">
-              <Grid item>
-                <span style={{ fontWeight: 'bold', marginBottom: isMobile ? '1rem' : '0rem', display: isMobile ? 'block' : 'inline' }}>
-                  {
-                    tabSelected == 0 ? "WALLET" :
-                      tabSelected == 1 ? "SEND ASSETS" :
-                        tabSelected == 2 ? "SECURITY & SETTINGS" : "SETTINGS"
-                  }
-                </span>
-              </Grid>
-              <Grid item>
-                <Grid container direction="row"
-                  justify="flex-end"
-                  alignItems="center"
-                  spacing={1}>
-                  <Grid item>
-                    <span style={{ fontWeight: 'bold' }}>
-                      Network:
-                    </span>
-                  </Grid>
-                  <Grid item>
-                    <FormControl variant="outlined" className={classes.formControl}>
-                      <Select
-                        value={network}
-                        onChange={handleChange}
-                        style={{ height: '2rem' }}
-                      >
-                        <MenuItem value='MainNet'>MainNet</MenuItem>
-                        <MenuItem value='TestNet'>TestNet</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </Grid>
-                </Grid>
-              </Grid>
+    <div style={{ minHeight: '100vh', background: '#21242c', color: 'white', paddingBottom: '5rem' }}>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item style={{
+          width: '100%',
+          fontSize: '20px',
+          backgroundColor: '#2d2d37',
+          color: '#fff',
+          padding: '1rem',
+        }}>
+          <Grid container direction="row" justify="space-between" alignItems="center">
+            <Grid item xs>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <Select
+                  value={network}
+                  onChange={handleChange}
+                  style={{
+                    height: '2rem',
+                    background: 'white',
+                    borderRadius: '4px'
+                  }}
+                >
+                  <MenuItem value='MainNet'>MainNet</MenuItem>
+                  <MenuItem value='TestNet'>TestNet</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-            <Divider style={{ margin: '1rem 0' }} />
-            <Grid container direction="row" justify={isMobile ? 'center' : "flex-start"} alignItems="flex-start" spacing={3}>
-              <Grid item>
-                <Grid container direction="column" justify="center" alignItems="center">
-                  <Grid item>
-                    <WalletView icon={walletIcon} onClick={() => setTabSelected(0)} />
-                  </Grid>
-                  <Grid item>
-                    <span style={{ fontWeight: tabSelected == 0 ? 'bold' : 'normal' }}>My wallet</span>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item>
-                <Grid container direction="column" justify="center" alignItems="center">
-                  <Grid item>
-                    <WalletView icon={sendIcon} onClick={() => setTabSelected(1)} />
-                  </Grid>
-                  <Grid item>
-                    <span style={{ fontWeight: tabSelected == 1 ? 'bold' : 'normal' }}>Send</span>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item>
-                <Grid container direction="column" justify="center" alignItems="center">
-                  <Grid item>
-                    <WalletView icon={keyIcon} onClick={() => setTabSelected(2)} />
-                  </Grid>
-                  <Grid item>
-                    <span style={{ fontWeight: tabSelected == 2 ? 'bold' : 'normal' }}>{'Security & settings'}</span>
-                  </Grid>
-                </Grid>
-              </Grid>
-              {/* <Grid item>
-                <Grid container direction="column" justify="center" alignItems="center">
-                  <Grid item>
-                    <WalletView icon={settingsIcon} onClick={() => setTabSelected(3)} />
-                  </Grid>
-                  <Grid item>
-                    <span style={{ fontWeight: tabSelected == 3 ? 'bold' : 'normal' }}>Settings</span>
-                  </Grid>
-                </Grid>
-              </Grid> */}
+            <Grid item xs style={{ textAlign: 'center' }}>
+              {isMobile ? '' : 'WALLET'}
             </Grid>
-            <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={3}>
-              <Grid item style={{ padding: '21px', width: '100%' }}>
-                {
-                  tabSelected == 0 ?
-                    <MyWallet network={network} />
-                    : tabSelected == 1 ?
-                      <SendWallet network={network} />
-                      : tabSelected == 2 ?
-                        <SecurityWallet isMobile={isMobile} />
-                        : null
-                }
-              </Grid>
+            <Grid item xs style={{ textAlign: 'end' }}>
+              <SvgIcon style={{ cursor: 'pointer' }} onClick={() => window.open(server.includes("localhost") ? server + "?settings=true" : server + "?settings=true/", 'NeoLogin - Login', 'width=400,height=660')}>
+                <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" />
+              </SvgIcon>
             </Grid>
-          </Paper>
+          </Grid>
+        </Grid>
+        <Grid item style={{ width: '100%' }}>
+          <Grid container direction='column' justify="flex-start" alignItems="center">
+            {
+              isMobile ?
+                <Grid item>
+                  <h3>WALLET</h3>
+                </Grid>
+                : null
+            }
+            <Grid item style={{ margin: isMobile ? '1rem 0 1rem 0' : '3rem 0 1rem 0' }}>
+              <canvas id="addressQR" />
+            </Grid>
+            <Grid item style={{ marginBottom: isMobile ? '1.5rem' : '3rem' }}>
+              <span>{address}</span>
+            </Grid>
+            {
+              mainBalance.length != 0 ?
+                mainBalance.map((balance) =>
+                  <Grid item style={{ width: isMobile ? '90%' : '50%', margin: '0.3rem 0rem' }}>
+                    <AssetRow symbol={balance.symbol} amount={balance.amount} address={address} network={network} getBalance={() => this.getBalance(address, network)} />
+                  </Grid>
+                )
+                :
+                address == '' ? null :
+                  <Grid item style={{ margin: '0.3rem 0rem' }}>
+                    <p>You have nothing:)</p>
+                  </Grid>
+            }
+          </Grid>
         </Grid>
       </Grid>
     </div >
---
+++ wallet/src/Components/AssetRow.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+import copyIcon from '../imgs/copy.png'
+
+import neologin from 'neologin';
+
+
+import Collapse from '@material-ui/core/Collapse';
+import './assetrow.css'
+
+const ASSETS = {
+    'NEO':
+    {
+        name: 'NEO',
+        icon: <path d="M207.288 35.521L132.161 0 21.651 39.9l75.127 35.521 110.51-39.9zm-77.066 42.211v73.601l78.278 36.982V49.471l-78.278 28.261zM94.839 89.441V227L18.5 190.991V53.348"></path>
+    },
+    'GAS':
+    {
+        name: 'GAS',
+        icon: <path d="M207.288 35.521L132.161 0 21.651 39.9l75.127 35.521 110.51-39.9zm-77.066 42.211v73.601l78.278 36.982V49.471l-78.278 28.261zM94.839 89.441V227L18.5 190.991V53.348"></path>
+    }
+}
+
+class AssetRow extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false,
+            toAddress: '',
+            amount: ''
+        };
+    }
+
+    unCollapse = () => {
+        this.setState({ open: !this.state.open })
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    send = () => {
+        neologin.send({
+            fromAddress: this.props.address,
+            toAddress: this.state.toAddress,
+            asset: this.props.symbol,
+            amount: this.state.amount,
+            remark: 'Hash puppy clothing purchase. Invoice#abc123',
+            fee: '0.0001',
+            network: this.props.network,
+            broadcastOverride: false,
+        })
+            .then(({ txid, nodeUrl }) => {
+                this.setState({ open: false })
+                console.log('Send transaction success!');
+                console.log('Transaction ID: ' + txid);
+                console.log('RPC node URL: ' + nodeUrl);
+                this.props.getBalance()
+            })
+            .catch(({ type, description, data }) => {
+                switch (type) {
+                    case "NO_PROVIDER":
+                        console.log('No provider available.');
+                        break;
+                    case "SEND_ERROR":
+                        console.log('There was an error when broadcasting this transaction to the network.');
+                        break;
+                    case "MALFORMED_INPUT":
+                        console.log('The receiver address provided is not valid.');
+                        break;
+                    case "CANCELED":
+                        console.log('The user has canceled this transaction.');
+                        break;
+                    case "INSUFFICIENT_FUNDS":
+                        console.log('The user has insufficient funds to execute this transaction.');
+                        break;
+                }
+            });
+
+    }
+
+    render() {
+        return (
+            <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="flex-start"
+            >
+                <Grid item style={{ width: '100%' }}>
+                    <Grid container direction="row" justify="space-between" alignItems="center" style={{
+                        background: '#2d2d37',
+                        padding: '8px 15px',
+                        borderRadius: this.state.open ? '5px 5px 0px 0px' : '5px',
+                        cursor: 'pointer',
+                        transitionDelay: !this.state.open ? '281ms' : '0ms'
+                    }}
+                        onClick={this.unCollapse}
+                    >
+                        <Grid item>
+                            <div class="badge">
+                                <div class="crypto">
+                                    <svg width="20" height="20" viewBox="0 0 226 226">
+                                        {ASSETS[this.props.symbol].icon}
+                                    </svg>
+                                </div>
+                            </div>
+                        </Grid>
+                        <Grid item xs style={{ paddingLeft: '15px' }}>
+                            <span style={{ display: 'block' }}>{ASSETS[this.props.symbol].name}</span>{/* 
+                    <span style={{display:'block'}}>BTC</span> */}
+                        </Grid>
+                        <Grid item>{this.props.amount} {this.props.symbol}</Grid>
+                    </Grid>
+                </Grid>
+                <Collapse in={this.state.open} timeout="auto" style={{ width: '100%' }}>
+                    <Grid item xs style={{ width: '100%' }}>
+                        <Grid container direction="column" style={{
+                            background: '#2d2d37',
+                            padding: '8px 15px',
+                            alignItems: 'center'
+                        }}>
+                            <Grid item>
+                                <p className="label" style={{ margin: 0 }}>SEND ASSET TO</p>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item style={{ width: '100%' }}>
+                        <Grid container direction="row" style={{
+                            background: '#2d2d37',
+                            padding: '8px 15px'
+                        }}>
+                            <Grid item xs>
+                                <label className="label" for="toAddress">address</label>
+                                <input type="text" name="toAddress" id="toAddress" onChange={this.handleInputChange} />
+                            </Grid>
+                            <Grid item style={{ width: '1rem' }}>
+                            </Grid>
+                            <Grid item>
+                                <label className="label" for="amount">Amount</label>
+                                <input type="text" name="amount" id="amount" onChange={this.handleInputChange} />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item style={{ width: '100%' }}>
+                        <Grid container
+                            direction="row"
+                            justify="flex-end"
+                            alignItems="center" style={{
+                                background: '#2d2d37',
+                                padding: '8px 15px',
+                                borderRadius: '0px 0px 5px 5px'
+                            }}>
+                            <Grid item style={{ marginBottom: '0.5rem' }}>
+                                <input type="submit" value="Send" class="primary" onClick={this.send} />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Collapse>
+            </Grid >
+        )
+    }
+}
+
+export default AssetRow
