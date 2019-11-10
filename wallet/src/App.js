d1972cac90f24f37d824ba9c09ebe067b28a5e75
urlchange <salah.karim@e-campus.uab.cat>
2019-11-10 16:12:48 +0800

    issues link /wallet

--- wallet/src/App.js
+++ wallet/src/App.js
@@ -6,13 +6,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import QRCode from 'qrcode'
 import SvgIcon from '@material-ui/core/SvgIcon';
-
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import AssetRow from './Components/AssetRow'
+import Menu from '@material-ui/core/Menu';
+import Link from '@material-ui/core/Link';
 
 import neologin from 'neologin'
-
-import AssetRow from './Components/AssetRow'
 
 import { server } from './config';
 
@@ -84,7 +84,15 @@ function App() {
       })
   }, [])
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   function getBalance(address, network) {
     neologin.getBalance({
@@ -144,9 +152,29 @@ function App() {
               {isMobile ? '' : 'WALLET'}
             </Grid>
             <Grid item xs style={{ textAlign: 'end' }}>
-              <SvgIcon style={{ cursor: 'pointer' }} onClick={() => window.open(server.includes("localhost") ? server + "?settings=true" : server + "?settings=true/", 'NeoLogin - Login', 'width=400,height=660')}>
-                <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" />
-              </SvgIcon>
+              <FormControl variant="outlined" className={classes.formControl} style={{ alignItems: 'flex-end' }}>
+                <SvgIcon aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} style={{ cursor: 'pointer' }}>
+                  <path d="M9 5.5c.83 0 1.5-.67 1.5-1.5S9.83 2.5 9 2.5 7.5 3.17 7.5 4 8.17 5.5 9 5.5zm0 2c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5S9.83 7.5 9 7.5zm0 5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5z" />
+                </SvgIcon>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={() => {
+                    handleClose()
+                    window.open(server.includes("localhost") ? server + "?settings=true" : server + "?settings=true/", 'NeoLogin - Login', 'width=400,height=660')
+                  }}>Settings</MenuItem>
+                  <MenuItem onClick={() => {
+                    handleClose()
+                    window.open("https://github.com/safudex/neologin/issues")
+                  }}>
+                    Issues
+                  </MenuItem>
+                </Menu>
+              </FormControl>
             </Grid>
           </Grid>
         </Grid>
