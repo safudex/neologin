8e9e5430be0cbc79bcc9f7106954a672e7e4a6a4
Albert <github@albert.sh>
2019-11-19 05:27:42 +0800

    Fix merge & deactivate deposit

--- widget/src/Views/InsufficientFunds.js
+++ widget/src/Views/InsufficientFunds.js
@@ -51,7 +51,6 @@ class InsufficientFunds extends React.Component {
     return (
       <>
         <Brand closeWidget={() => { this.props.reject(); this.unmountComponent(); }} reqNumber={parseInt(this.props.contid.split('-')[1]) + 1} />
-<<<<<<< HEAD
         {
           !this.state.wantsDeposit ?
             < div >
@@ -70,7 +69,7 @@ class InsufficientFunds extends React.Component {
                   <span style={{ fontSize: '0.85em' }}>{this.props.address}</span>
                 </Grid>
                 <Grid item xs>
-					<p style={{ fontSize: '0.85em' }}>{t("info_notFounds")}</p>
+                  <p style={{ fontSize: '0.85em' }}>{t("info_notFounds")}</p>
                 </Grid>
                 <Grid item style={{ width: '100%', marginBottom: '15px' }}>
                   <Grid container direction='row' alignItems='center' justify='center' style={{ width: '100%', textAlign: 'center' }}>
@@ -88,9 +87,9 @@ class InsufficientFunds extends React.Component {
                 <Grid item xs style={{ width: '100%' }}>
                   <button className='buttonContinue' onClick={() => {
                     console.log(this.props.network)
-                    this.props.network === 'MainNet' ? this.setState({ wantsDeposit: true }) : window.open('https://neowish.ngd.network/', '_blank')
+                    this.props.network === 'MainNet' ? this.setState({ wantsDeposit: false }) : window.open('https://neowish.ngd.network/', '_blank')
                   }}>
-                    {this.props.network === 'MainNet' ? 'Deposit' : 'Get free Testnet assets'}
+                    {this.props.network === 'MainNet' ? 'Deposit [SOON]' : 'Get free Testnet assets'}
                   </button>
                 </Grid>
               </Grid >
@@ -100,26 +99,6 @@ class InsufficientFunds extends React.Component {
 
         }
       </>
-=======
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          style={{ height: '100%', padding: '1em' }}
-        >
-          <Grid item xs>
-            <img alt="address" style={{ height: '150px' }} src={"https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=" + this.props.address + "&chld=L|0"} />
-          </Grid>
-          <Grid item xs>
-            <span style={{ fontSize: '0.85em' }}>{this.props.address}</span>
-          </Grid>
-          <Grid item xs>
-            <p style={{ fontSize: '0.85em' }}>{t("info_notFounds")}</p>
-          </Grid>
-        </Grid>
-      </div >
->>>>>>> implemented translations widget +
     );
   }
 }
